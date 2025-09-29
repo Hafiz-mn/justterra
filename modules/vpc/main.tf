713ca031ab1b7a6cc956061af185e69085768b88
Hafiz-mn <hafizmohammedmn3@gmail.com>
2025-09-29 20:49:33 +0530

    module directory corrected

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,32 +1,13 @@
-resource "aws_vpc" "this" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name             = var.vpc_name
+  cidr             = var.vpc_cidr
+  azs              = var.azs
+  private_subnets  = var.private_subnets
+  public_subnets   = var.public_subnets
+
   enable_dns_hostnames = true
-}
-
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-}
-
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = var.az
-  map_public_ip_on_launch = true
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-}
-
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
 }
 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,16 +1,25 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  type        = string
+variable "azs" {
+  description = "List of Availability Zones"
+  type        = list(string)
 }
 
-variable "az" {
-  description = "Availability Zone for the subnet"
-  type        = string
+variable "private_subnets" {
+  description = "Private subnets"
+  type        = list(string)
 }
 
+variable "public_subnets" {
+  description = "Public subnets"
+  type        = list(string)
+}
 
