fa30c4c88f58c4034b2b2904eb85c9a4277fe701
Hafiz-mn <hafizmohammedmn3@gmail.com>
2025-09-29 17:09:41 +0530

    module created-VPC AND ECS

---
+++ terraform-ecs-envs/modules/ecs/variables.tf
@@ -0,0 +1,16 @@
+variable "cluster_name" {}
+variable "task_family" {}
+variable "cpu" {}
+variable "memory" {}
+variable "container_name" {}
+variable "container_image" {}
+variable "container_port" {}
+variable "service_name" {}
+variable "desired_count" {}
+variable "subnets" {}
+variable "alb_name" {}
+variable "alb_subnets" {}
+variable "alb_vpc_id" {}
+variable "alb_sg_name" {}
+variable "target_group_name" {}
+
---
+++ terraform-ecs-envs/modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of Availability Zones"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnets"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnets"
+  type        = list(string)
+}
+
